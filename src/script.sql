5ed5b217d90594e71d0728be9fadec97abf9a5b5
Marcos Henrique Lopes da Silva <111400765+MarcosocraM1@users.noreply.github.com>
2023-09-18 08:38:29 -0300

    Update script.sql

--- src/script.sql
+++ src/script.sql
@@ -1,45 +1,69 @@
-drop table cliente;
-drop table produto;
-drop table endereco;
-drop table fornecedor;
+drop table Cliente;
+drop table Produto;
+drop table Endereco;
+drop table Fornecedor;
 
-create table cliente(
-    nome varchar2(50),
-    cpf varchar2(50),
+create table Cliente(
+    nome varchar2(100),
+    cpf varchar2(100),
     email varchar2(100),
+    id_endereco int,
     constraint cpf_pk primary key (cpf)
 );
 
 
-create table produto(
+create table Produto(
     nome varchar2(100),
     id_produto int,
     qtd_produto int,
     marca varchar2(100),
     categoria varchar2(100),
     preco int,
+    cnpj_forncedor varchar2(100),
     constraint id_produto_pk primary key (id_produto)
 );
 
 
-create table fornecedor(
+create table Fornecedor(
     cnpj varchar2(100),
     nome varchar2(100),
+    id_endereco int,
     constraint cnpj_pk primary key (cnpj)
 );
 
 
-create table endereco(
+create table Endereco(
+    id_endereco int,
     numero int,
     rua varchar2(100),
     bairro varchar2(100),
     cidade varchar2(100),
     pais varchar2(100),
-    constraint numero_pk primary key (numero)
+    constraint id_endereco_pk primary key (id_endereco)
 );
 
 
+alter table Cliente
+add constraint cliente_fk foreign key (id_endereco)
+    references Endereco (id_endereco);
+
+alter table Produto
+add constraint produto_fk foreign key (cnpj_forncedor)
+    references Fornecedor (cnpj);
+
+alter table Fornecedor
+add constraint fornecedor_fk foreign key (id_endereco)
+    references Endereco (id_endereco);
+
+
+
 create sequence seq_produto
+start with 1
+increment by 1
+nocache
+nocycle;
+
+create sequence seq_endereco
 start with 1
 increment by 1
 nocache
