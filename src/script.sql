6901c6a20deab5941cb52af5583f6cc6557e568a
Marcos Henrique Lopes da Silva <111400765+MarcosocraM1@users.noreply.github.com>
2023-09-18 07:50:38 -0300

    Update script.sql

--- src/script.sql
+++ src/script.sql
@@ -1,20 +1,41 @@
+drop table cliente;
+drop table produto;
+drop table endereco;
+drop table fornecedor;
+
 create table cliente(
     nome varchar2(50),
     cpf varchar2(50),
     email varchar2(100),
-    cidade varchar2(50),
-    bairro varchar2(50),
-    rua varchar2(50),
-    numero int,
     constraint cpf_pk primary key (cpf)
 );
 
 
 create table produto(
-    nome varchar2(50),
+    nome varchar2(100),
     id_produto int,
     qtd_produto int,
+    marca varchar2(100),
+    categoria varchar2(100),
+    preco int,
     constraint id_produto_pk primary key (id_produto)
+);
+
+
+create table fornecedor(
+    cnpj varchar2(100),
+    nome varchar2(100),
+    constraint cnpj_pk primary key (cnpj)
+);
+
+
+create table endereco(
+    numero int,
+    rua varchar2(100),
+    bairro varchar2(100),
+    cidade varchar2(100),
+    pais varchar2(100),
+    constraint numero_pk primary key (numero)
 );
 
 
