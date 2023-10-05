61b31c906b1a333718bf1218583a5aa272da02ff
Marcos Henrique Lopes da Silva <111400765+MarcosocraM1@users.noreply.github.com>
2023-10-05 07:47:47 -0300

    Update script.sql

--- src/script.sql
+++ src/script.sql
@@ -1,14 +1,29 @@
-drop table Cliente;
-drop table Produto;
-drop table Endereco;
-drop table Fornecedor;
 
 create table Cliente(
     nome varchar2(100),
     cpf varchar2(100),
     email varchar2(100),
-    id_endereco int,
+    estado varchar2(100),
+    rua varchar2(100),
+    numero number,
+    bairro varchar2(100),
+    cidade varchar2(100),
+    cep varchar2(100),
+    pais varchar2(100),
     constraint cpf_pk primary key (cpf)
+);
+
+create table Fornecedor(
+    cnpj varchar2(100),
+    nome varchar2(100),
+    estado varchar2(100),
+    rua varchar2(100),
+    numero number,
+    bairro varchar2(100),
+    cidade varchar2(100),
+    cep varchar2(100),
+    pais varchar2(100),
+    constraint cnpj_pk primary key (cnpj)
 );
 
 
@@ -20,41 +35,9 @@ create table Produto(
     categoria varchar2(100),
     preco int,
     cnpj_forncedor varchar2(100),
-    constraint id_produto_pk primary key (id_produto)
+    constraint id_produto_pk primary key (id_produto),
+    constraint produto_fk foreign key (cnpj_forncedor)references Fornecedor (cnpj)
 );
-
-
-create table Fornecedor(
-    cnpj varchar2(100),
-    nome varchar2(100),
-    id_endereco int,
-    constraint cnpj_pk primary key (cnpj)
-);
-
-
-create table Endereco(
-    id_endereco int,
-    numero int,
-    rua varchar2(100),
-    bairro varchar2(100),
-    cidade varchar2(100),
-    pais varchar2(100),
-    constraint id_endereco_pk primary key (id_endereco)
-);
-
-
-alter table Cliente
-add constraint cliente_fk foreign key (id_endereco)
-    references Endereco (id_endereco);
-
-alter table Produto
-add constraint produto_fk foreign key (cnpj_forncedor)
-    references Fornecedor (cnpj);
-
-alter table Fornecedor
-add constraint fornecedor_fk foreign key (id_endereco)
-    references Endereco (id_endereco);
-
 
 
 create sequence seq_produto
@@ -63,7 +46,15 @@ increment by 1
 nocache
 nocycle;
 
-create sequence seq_endereco
+
+
+create sequence seq_cliente
+start with 1
+increment by 1
+nocache
+nocycle;
+
+create sequence seq_fornecedor
 start with 1
 increment by 1
 nocache
